140af8ca0d7cbefcb06915d6dbc34d4a478b9e89
Sistahana7 <92984956+Sistahana7@users.noreply.github.com>
2022-11-14 13:27:30 +0700

    DataKaryawanSista

---
+++ Karyawan.js
@@ -0,0 +1,23 @@
+const karyawan = [
+    {id:'1', nama:'Sistahana Sari Dewi', jabatan:'CEO',
+    email:'sistahana@gmail.com', telp:'081333335566',
+    gaji:'40.000.000', foto:'https://placeimg.com/100/100/people'},
+    
+    {id:'2', nama:'Nora Cedricc', jabatan:'Manager',
+    email:'nornor@gmail.com', telp:'081323456655',
+    gaji:'15.000.000', foto:'https://placeimg.com/100/100/nature'},
+
+    {id:'3', nama:'Min Yoongi', jabatan:'Produser',
+    email:'yoongimin@gmail.com', telp:'081234567788',
+    gaji:'50.000.000', foto:'https://placeimg.com/100/100/tech'},
+
+    {id:'4', nama:'Mario Maurer', jabatan:'Arsitek',
+    email:'maurerm@gmail.com', telp:'081323335454',
+    gaji:'5.000.000', foto:'https://placeimg.com/100/100/arch'},
+
+    {id:'5', nama:'Adinda Putri', jabatan:'Insinyur Pertambangan',
+    email:'putriadin@gmail.com', telp:'081300089871',
+    gaji:'7.500.000', foto:'https://placeimg.com/100/100/any/grayscale'},
+];
+
+export default karyawan;
